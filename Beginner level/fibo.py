a5ca68b16caf66b188d28173171ebaadc774bf45
GAYUGAYA <37337237+GAYUGAYA@users.noreply.github.com>
2018-04-12 18:51:59 +0530

    Create fibo.py

---
+++ Beginner level/fibo.py
@@ -0,0 +1,16 @@
+Number=int(input("enter the number"))
+i=0
+n1=0
+n2=1
+while(i < Number):
+           if(i <= 1):
+                      Next = i
+           else:
+                      Next=n1+n2
+                      n1=n2
+                      n2=Next
+           print(Next)
+           i=i+1
+
+
+
